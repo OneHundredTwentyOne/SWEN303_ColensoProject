de3dfd12e81002c8c5ae80188b3ca6b2c26e33f6
thepangman <azzapang40@yahoo.com>
2016-03-19 19:02:38 +1300

    Modified index.js with baseX code

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
+var basex = require('basex');
+var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
+client.execute("OPEN Colenso");
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/",function(req,res){
+client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
+" (//name[@type='place'])[1] ",
+function (error, result) {
+  if(error){ console.error(error);}
+  else {
+    res.render('index', { title: 'ColensoProject', place: result.result });
+}
+      }
+      );
 });
+
 
 module.exports = router;
